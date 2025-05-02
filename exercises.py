a3c2bebc59f1e3f608ee5214274f93dfedf2276a
annams <annaextrasteckel@gmail.com>
2025-05-01 18:28:54 -0700

    exercise 2 complete

--- exercises.py
+++ exercises.py
@@ -70,3 +70,39 @@ check_letter()
 # https://brainly.com/question/22294497
 
 #-----------------------------------------------------------------------------------------------------
+
+# Exercise 2: Old enough to vote?
+#
+# Write a Python function named `check_voting_eligibility` that determines if a user is old enough to vote.
+# Fill in the logic to perform the eligibility check inside the function.
+#
+# Function Details:
+# - Prompt the user to input their age: "Please enter your age: "
+# - Validate the input to ensure the age is a possible value (no negative numbers).
+# - Determine if the user is eligible to vote. Set a variable for the voting age.
+# - Print a message indicating whether the user is eligible to vote based on the entered age.
+#
+# Hints:
+# - Use the `input()` function to capture the user's age.
+# - Use `int()` to convert the input to an integer. Ensure to handle any conversion errors gracefully.
+# - Use a conditional statement to check if the age meets the minimum voting age requirement.
+
+def check_voting_eligibility():
+    # Your control flow logic goes here
+    age = input('Enter your age to see if you\'re old enough to vote: ')
+    voting_age = 18
+    
+    if int(age) <= 0:
+        print('Oops! Try adding a valid age next time.')
+    elif int(age) >= voting_age:
+        print('Looks like you\'re old enough to vote!')
+    else:
+        print('You\'ve still got a few years before you can vote!')
+
+# Call the function
+check_voting_eligibility()
+
+# https://www.w3schools.com/python/ref_func_int.asp
+
+#-----------------------------------------------------------------------------------------------------
+
